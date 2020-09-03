46bfd72cba7ff00feb73123765d3abc0b0cdfbea
v.moroz@firsttacs.com <nBevvrPhjPc3MM1WxSTT>
2020-09-03 17:39:45 +0300

    init app

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <button>Left button</button>
+
+    <button>Right button</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  display: flex;
+  justify-content: space-between;
+}
+
+  button {
+    cursor: pointer;
+    padding: 10px;
+  }
+
+</style>
